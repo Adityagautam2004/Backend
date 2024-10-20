52181b917334cd635a20af408ab6b4b7c2f20694
Adityagautam2004 <adikr2002@gmail.com>
2024-10-20 20:07:26 +0530

    cloudinary bug fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,10 +1,6 @@
 import {v2 as cloudinary} from "cloudinary"
 import fs  from "fs"
 
-import { v2 as cloudinary } from 'cloudinary';
-import { log } from "console";
-
-
     cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY, 
@@ -25,6 +21,8 @@ import { log } from "console";
         } catch (error) {
             fs.unlinkSync(localFilePath)
             //remove the locally saved temporary files as the upload operation got failed
-            return null;
+            return null
         }
     }
+
+    export{uploadOnCloudinary}
