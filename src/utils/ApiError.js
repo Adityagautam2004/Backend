ab70fbabc0e8eeb892c48515f990166ee5736527
Adityagautam2004 <adikr2002@gmail.com>
2024-10-20 16:54:11 +0530

    all setup configured

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -2,8 +2,8 @@ class ApiError extends Error{
     constructor(
         statusCode,
         message = "Something went Wrong",
-        error = [],
-        statck =""
+        errors = [],
+        stack =""
     ){
         super(message)
         this.statusCode = statusCode
